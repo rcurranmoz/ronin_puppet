334d27d410a7c3669df0dda30528f75dbec88a4b
Ryan Curran <rcurran@mozilla.com>
2025-05-23 13:37:35 -0400

    test site.pp change

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,7 +6,7 @@
 #   variables like this should be set in profile modules.
 #   these make testing difficult (this block must be
 #   copy/pasted into every puppet-kitchen manifests).
-case $::operatingsystem {
+case $facts['os']['name'] {
     'Windows': {
     }
     'Darwin': {
@@ -21,6 +21,23 @@ case $::operatingsystem {
     }
     default: {
     }
+}
+
+# Role-based classification using puppet_role fact
+if $facts['puppet_role'] {
+  case $facts['puppet_role'] {
+    'gecko_t_osx_1400_r8_staging': {
+      include ::roles_profiles::roles::gecko_t_osx_1400_r8_staging
+    }
+    'mozilla_b_1_osx': {
+      include ::roles_profiles::roles::mozilla_b_1_osx
+    }
+    default: {
+      fail("Unknown puppet_role: ${facts['puppet_role']}")
+    }
+  }
+} else {
+  fail('No puppet_role fact provided. Cannot classify node.')
 }
 
 # node /vagrantup.com/ {
